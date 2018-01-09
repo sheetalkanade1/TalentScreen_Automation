8b6c70dda0331b9f90cc9c0ac6b9dab8e43d0d29
sheetalkanade1 <31222682+sheetalkanade1@users.noreply.github.com>
2018-01-08 21:20:38 -0500

    Update BaseTest.java

--- TalentScreen_Automation/src/main/java/com/wbl/base/BaseTest.java
+++ TalentScreen_Automation/src/main/java/com/wbl/base/BaseTest.java
@@ -3,7 +3,6 @@ import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
-//import org.testng.annotations.BeforeTest;
 import org.testng.annotations.BeforeTest;
 
 import com.wbl.utilities.ConfigUtils;
